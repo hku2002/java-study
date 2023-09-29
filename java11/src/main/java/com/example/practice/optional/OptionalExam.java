87da8068aeb6205f9e24d58d9f5d0b6103456e22
Hwang <hku2002@hanmail.net>
2023-09-29 21:19:43 +0900

    feat: 옵셔널 연습 추가

--- java11/src/main/java/com/example/practice/optional/OptionalExam.java
+++ java11/src/main/java/com/example/practice/optional/OptionalExam.java
@@ -1,5 +1,6 @@
 package com.example.practice.optional;
 
+import java.util.List;
 import java.util.Optional;
 
 public class OptionalExam {
@@ -18,5 +19,20 @@ public class OptionalExam {
         } catch (NullPointerException e) {
             e.printStackTrace();
         }
+
+        List<OfflineClass> offlineClasses = List.of(
+                new OfflineClass(1L, "Spring boot 2.0", true),
+                new OfflineClass(2L, "Spring boot 3.0", true),
+                new OfflineClass(3L, "rest api development", true)
+        );
+
+        // 옵셔널 받아오기
+        OfflineClass optionalClass = offlineClasses.stream()
+                .filter(offClass -> offClass.getTitle().startsWith("Spring"))
+                .findFirst()
+                .orElseThrow(IllegalStateException::new);
+        System.out.println(optionalClass.getTitle());
+
     }
+
 }
