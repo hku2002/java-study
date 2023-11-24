dab6f8dd40b38961d243ff0a898053e65d31636e
hku2002 <hku2002@hanmail.net>
2023-11-24 23:21:07 +0900

    feat: 최초 추가

---
+++ stock-synchronicity-exam/src/main/java/com/example/stocksynchronicityexam/StockSynchronicityExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.stocksynchronicityexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockSynchronicityExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockSynchronicityExamApplication.class, args);
+    }
+
+}
---
+++ stock-synchronicity-exam/src/test/java/com/example/stocksynchronicityexam/StockSynchronicityExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stocksynchronicityexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockSynchronicityExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
