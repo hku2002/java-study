93c132baa1ec77521a2e8b1fa797b620d8d54285
hku2002 <hku2002@hanmail.net>
2023-11-26 14:48:54 +0900

    feat: default 값 저장 추가

--- order-event-listner/src/main/java/com/example/order/common/entity/BaseEntity.java
+++ order-event-listner/src/main/java/com/example/order/common/entity/BaseEntity.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 public abstract class BaseEntity {
 
     @CreatedDate
-    @Column(updatable = false, nullable = false, columnDefinition = "DATETIME")
+    @Column(updatable = false, nullable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
     private LocalDateTime createdAt;
 
     @LastModifiedDate
