886dfd5ee6603641bb257098ec38244d01c3a33e
hku2002 <hku2002@hanmail.net>
2023-11-27 23:05:55 +0900

    refactor: 불필요한 코드 제거

--- stock-synchronicity-exam/src/test/java/com/example/stocksynchronicityexam/service/StockServiceTest.java
+++ stock-synchronicity-exam/src/test/java/com/example/stocksynchronicityexam/service/StockServiceTest.java
@@ -12,7 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -132,12 +131,10 @@ class StockServiceTest {
         ExecutorService executor = Executors.newFixedThreadPool(32);
         CountDownLatch countDownLatch = new CountDownLatch(threadCount);
 
-        AtomicInteger a = new AtomicInteger();
         for (int i = 0; i < threadCount; i++) {
             executor.submit(() -> {
                 try {
                     stockService.redisPubSubDecrease(1L, 1);
-                    a.getAndIncrement();
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
