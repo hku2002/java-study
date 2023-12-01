f9bf789e2ca195e88ee14675e3370a0d36d77a2d
hku2002 <hku2002@hanmail.net>
2023-12-01 18:06:55 +0900

    feat: 학생 한번 만들어보기

---
+++ basic/src/sample01/ClassStart1.java
@@ -0,0 +1,18 @@
+package sample01;
+
+public class ClassStart1 {
+
+    public static void main(String[] args) {
+        String student1Name = "학생1";
+        int student1Age = 17;
+        int student1Grade = 10;
+
+        String student2Name = "학생2";
+        int student2Age = 17;
+        int student2Grade = 20;
+
+        System.out.println("이름 : " + student1Name + ", 나이 : " + student1Age + " 성적 : " + student1Grade);
+        System.out.println("이름 : " + student2Name + ", 나이 : " + student2Age + " 성적 : " + student2Grade);
+    }
+
+}
