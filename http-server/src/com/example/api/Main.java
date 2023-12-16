59085d469d93900340e5c727a2f5b796c3b17d62
hku2002 <hku2002@hanmail.net>
2023-12-16 17:47:18 +0900

    feat: ServerManager 추가

--- http-server/src/com/example/api/Main.java
+++ http-server/src/com/example/api/Main.java
@@ -1,9 +1,19 @@
 package com.example.api;
 
+import com.example.api.config.HttpServerManager;
+
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("simple http server");
+
+        HttpServerManager serverManager = new HttpServerManager();
+        serverManager.start();
+
+        serverManager.stop();
+
     }
 
 }
---
+++ http-server/src/com/example/api/config/HttpServerManager.java
@@ -0,0 +1,29 @@
+package com.example.api.config;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class HttpServerManager {
+
+    private final HttpServer httpServer;
+
+    public HttpServerManager() throws IOException {
+        httpServer = HttpServer.create(new InetSocketAddress("127.0.0.1", 8080), 0);
+        httpServer.createContext("/"); // TODO: add Handler
+    }
+
+    public void start() {
+        httpServer.start();
+        System.out.println("서버가 실행되었습니다.");
+    }
+
+    public void stop() {
+        System.out.println("3초 후 서버가 종료됩니다.");
+        httpServer.stop(3);
+        System.out.println("서버가 종료되었습니다.");
+    }
+
+
+}
