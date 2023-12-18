f63d3acfdadd1613847b9cd9454f2bdca7097246
hku2002 <hku2002@hanmail.net>
2023-12-18 23:11:02 +0900

    fix: LocalDate -> Date 변경

--- mysql-performance-optimization/src/main/java/com/example/mysqlperformanceoptimization/domain/entity/Order.java
+++ mysql-performance-optimization/src/main/java/com/example/mysqlperformanceoptimization/domain/entity/Order.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
@@ -16,13 +17,13 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long orderId;
     private Long customerId;
-    private LocalDateTime orderDate;
+    private LocalDate orderDate;
     private Long productId;
     private int quantity;
     private String status;
 
     @Builder
-    public Order(Long orderId, Long customerId, LocalDateTime orderDate, Long productId, int quantity, String status) {
+    public Order(Long orderId, Long customerId, LocalDate orderDate, Long productId, int quantity, String status) {
         this.orderId = orderId;
         this.customerId = customerId;
         this.orderDate = orderDate;
