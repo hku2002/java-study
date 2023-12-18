1685f1d9122dd2fa3e3723cec1ea568e8756f704
hku2002 <hku2002@hanmail.net>
2023-12-18 22:44:15 +0900

    feat: mysql 성능 최적화 코드 repo 최초 추가

---
+++ mysql-performance-optimization/src/main/java/com/example/mysqlperformanceoptimization/MysqlPerformanceOptimizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysqlperformanceoptimization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlPerformanceOptimizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlPerformanceOptimizationApplication.class, args);
+    }
+
+}
---
+++ mysql-performance-optimization/src/test/java/com/example/mysqlperformanceoptimization/MysqlPerformanceOptimizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysqlperformanceoptimization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlPerformanceOptimizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
