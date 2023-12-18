e808a463f22934bbad0e45346a857c2c10f29d3b
hku2002 <hku2002@hanmail.net>
2023-12-18 23:06:33 +0900

    feat: Order 생성

---
+++ mysql-performance-optimization/src/main/java/com/example/mysqlperformanceoptimization/domain/entity/Order.java
@@ -0,0 +1,33 @@
+package com.example.mysqlperformanceoptimization.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+    private Long customerId;
+    private LocalDateTime orderDate;
+    private Long productId;
+    private int quantity;
+    private String status;
+
+    @Builder
+    public Order(Long orderId, Long customerId, LocalDateTime orderDate, Long productId, int quantity, String status) {
+        this.orderId = orderId;
+        this.customerId = customerId;
+        this.orderDate = orderDate;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.status = status;
+    }
+}
