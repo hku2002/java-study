45f0aaebf2a5d2cd231ef28fe58764d1533e44e3
hku2002 <hku2002@hanmail.net>
2024-06-28 21:03:04 +0900

    refactor: 계산로직 메소드화

--- practice-refactoring/src/main/java/com/example/insurance/InsuranceCalculator.java
+++ practice-refactoring/src/main/java/com/example/insurance/InsuranceCalculator.java
@@ -4,15 +4,19 @@ public class InsuranceCalculator {
 
     public double calculateInsurance(double income) {
         if (isGrateAndEquals(income, 10000)) {
-            return income * 0.365 + 0;
+            return calculateCommand(income, 0, 0.365, 0);
         }
         if (isGrateAndEquals(income, 30000)) {
-            return (income - 10000) * 0.2 + 35600;
+            return calculateCommand(income, 10000, 0.2, 35600);
         }
         if (isGrateAndEquals(income, 60000)) {
-            return (income - 30000) * 0.1 + 76500;
+            return calculateCommand(income, 30000, 0.1, 76500);
         }
-        return (income - 60000) * 0.02 + 105600;
+        return calculateCommand(income, 60000, 0.02, 105600);
+    }
+
+    private static double calculateCommand(double income, int adjustment, double weight, int constant) {
+        return (income - adjustment) * weight + constant;
     }
 
     private static boolean isGrateAndEquals(double income, int i) {
