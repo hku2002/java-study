be8fe31fcc7b8f23db0bdb9c408949d7f6a31481
hku2002 <hku2002@hanmail.net>
2024-06-28 22:21:41 +0900

    refactor: 필드로 위치 변경

--- practice-refactoring/src/main/java/com/example/insurance/InsuranceCalculator.java
+++ practice-refactoring/src/main/java/com/example/insurance/InsuranceCalculator.java
@@ -7,12 +7,13 @@ import static com.example.insurance.InsuranceStrategy.getInstance;
 public class InsuranceCalculator {
 
     public static final InsuranceStrategy DEFAULT_INSTANCE = getInstance(0, 60000, 0.02, 105600);
+    private final List<InsuranceStrategy> insuranceStrategies = List.of(
+            getInstance(10000, 0, 0.365, 0),
+            getInstance(30000, 10000, 0.2, 35600),
+            getInstance(60000, 30000, 0.1, 76500)
+    );
 
     public double calculateInsurance(double income) {
-        InsuranceStrategy instance1 = getInstance(10000, 0, 0.365, 0);
-        InsuranceStrategy instance2 = getInstance(30000, 10000, 0.2, 35600);
-        InsuranceStrategy instance3 = getInstance(60000, 30000, 0.1, 76500);
-        List<InsuranceStrategy> insuranceStrategies = List.of(instance1, instance2, instance3);
 
         for (InsuranceStrategy strategy : insuranceStrategies) {
             if (strategy.isGrateAndEquals(income)) {
