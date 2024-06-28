a4d9f0b57cd12a524b7ad28fc6a70ac81f0c73e7
hku2002 <hku2002@hanmail.net>
2024-06-28 21:43:03 +0900

    refactor: Parameter Object 생성

--- practice-refactoring/src/main/java/com/example/insurance/InsuranceCalculator.java
+++ practice-refactoring/src/main/java/com/example/insurance/InsuranceCalculator.java
@@ -4,22 +4,22 @@ public class InsuranceCalculator {
 
     public double calculateInsurance(double income) {
         if (isGrateAndEquals(income, 10000)) {
-            return calculateCommand(income, 0, 0.365, 0);
+            return calculateCommand(income, new insuranceStrategy(0, 0.365, 0));
         }
         if (isGrateAndEquals(income, 30000)) {
-            return calculateCommand(income, 10000, 0.2, 35600);
+            return calculateCommand(income, new insuranceStrategy(10000, 0.2, 35600));
         }
         if (isGrateAndEquals(income, 60000)) {
-            return calculateCommand(income, 30000, 0.1, 76500);
+            return calculateCommand(income, new insuranceStrategy(30000, 0.1, 76500));
         }
-        return calculateCommand(income, 60000, 0.02, 105600);
+        return calculateCommand(income, new insuranceStrategy(60000, 0.02, 105600));
     }
 
-    private static double calculateCommand(double income, int adjustment, double weight, int constant) {
-        return (income - adjustment) * weight + constant;
+    private double calculateCommand(double income, insuranceStrategy strategy) {
+        return (income - strategy.adjustment()) * strategy.weight() + strategy.constant();
     }
 
-    private static boolean isGrateAndEquals(double income, int baseIncome) {
+    private boolean isGrateAndEquals(double income, int baseIncome) {
         return income <= baseIncome;
     }
 
---
+++ practice-refactoring/src/main/java/com/example/insurance/insuranceStrategy.java
@@ -0,0 +1,4 @@
+package com.example.insurance;
+
+public record insuranceStrategy(int adjustment, double weight, int constant) {
+}
