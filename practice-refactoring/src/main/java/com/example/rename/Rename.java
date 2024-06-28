86528623418b2e884f35ce5e360c5c272d6460f1
hku2002 <hku2002@hanmail.net>
2024-06-28 20:37:44 +0900

    feat: rename 단축키 정리

---
+++ practice-refactoring/src/main/java/com/example/rename/Rename.java
@@ -0,0 +1,18 @@
+package com.example.rename;
+
+public class Rename {
+
+    public static void main(String[] args) {
+        /**
+         * rename 단축키: shift + F6
+         * 변수에 사용 시 모든 동일 명칭을 일괄 수정함
+         * class 에도 사용 가능
+         */
+        int temp = 1;
+        System.out.println("print variable: " + temp);
+
+        temp = 2;
+        System.out.println("print vatiable: " + temp);
+    }
+
+}
