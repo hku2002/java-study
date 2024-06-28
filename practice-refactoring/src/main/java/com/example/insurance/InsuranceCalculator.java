ae1da2a77a8f98572b4511be9f3c09f314c14a29
hku2002 <hku2002@hanmail.net>
2024-06-28 20:56:27 +0900

    refactor: 조건 메소드화

--- practice-refactoring/src/main/java/com/example/insurance/InsuranceCalculator.java
+++ practice-refactoring/src/main/java/com/example/insurance/InsuranceCalculator.java
@@ -3,16 +3,20 @@ package com.example.insurance;
 public class InsuranceCalculator {
 
     public double calculateInsurance(double income) {
-        if (income <= 10000) {
-            return income * 0.365;
+        if (isGrateAndEquals(income, 10000)) {
+            return income * 0.365 + 0;
         }
-        if (income <= 30000) {
+        if (isGrateAndEquals(income, 30000)) {
             return (income - 10000) * 0.2 + 35600;
         }
-        if (income <= 60000) {
+        if (isGrateAndEquals(income, 60000)) {
             return (income - 30000) * 0.1 + 76500;
         }
         return (income - 60000) * 0.02 + 105600;
+    }
+
+    private static boolean isGrateAndEquals(double income, int i) {
+        return income <= i;
     }
 
 }
