5aa1f48938010ca63556ca0f68a17a74c0fc69a6
hku2002 <hku2002@hanmail.net>
2024-07-05 20:50:27 +0900

    feat: PG 인스턴스 생성 팩토리 클래스 추가

---
+++ basic/src/pattern/strategy/PgFactory.java
@@ -0,0 +1,13 @@
+package pattern.strategy;
+
+import java.util.List;
+
+public class PgFactory {
+
+    public static final List<PgStrategy> pgStrategies = List.of();
+
+    public PgStrategy findStrategy(PgType pgType) {
+        return null;
+    }
+
+}
