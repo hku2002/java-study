649d961d6e72b1239f8eb2c6db0fdbe28dc0a40f
hku2002 <hku2002@hanmail.net>
2024-07-05 20:57:14 +0900

    refactor: record 로 수정

--- basic/src/pattern/strategy/Payment.java
+++ basic/src/pattern/strategy/Payment.java
@@ -1,16 +1,4 @@
 package pattern.strategy;
 
-public class Payment {
-
-    private final Long id;
-    private final PgType pgType;
-    private final int totalPay;
-    private final int userId;
-
-    public Payment(Long id, PgType pgType, int totalPay, int userId) {
-        this.id = id;
-        this.pgType = pgType;
-        this.totalPay = totalPay;
-        this.userId = userId;
-    }
+public record Payment(Long id, PgType pgType, int totalPay, int userId) {
 }
