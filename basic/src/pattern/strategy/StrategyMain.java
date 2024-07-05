43b1817b17b99aaa1f3cd5b0a541335c3f3dcd51
hku2002 <hku2002@hanmail.net>
2024-07-05 21:02:32 +0900

    feat: Client 에서 호출 로직 추가

--- basic/src/pattern/strategy/StrategyMain.java
+++ basic/src/pattern/strategy/StrategyMain.java
@@ -1,9 +1,25 @@
 package pattern.strategy;
 
+import static pattern.strategy.PgType.KAKAO_PAY;
+import static pattern.strategy.PgType.NAVER_PAY;
+
 public class StrategyMain {
 
     public static void main(String[] args) {
 
+        Payment naverPayment = createPayment(NAVER_PAY);
+        PgFactory pgFactory = new PgFactory();
+        PgStrategy naverStrategy = pgFactory.findStrategy(NAVER_PAY);
+        naverStrategy.requestPayment(naverPayment);
+
+        Payment payment = createPayment(KAKAO_PAY);
+        PgStrategy kakaoStrategy = pgFactory.findStrategy(KAKAO_PAY);
+        kakaoStrategy.requestPayment(payment);
+
+    }
+
+    private static Payment createPayment(PgType pgType) {
+        return new Payment(1L, pgType, 10_000, 1201);
     }
 
 }
