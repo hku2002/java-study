44ef11fb7457b78b4c472136a76a71d1c1966ead
hku2002 <hku2002@hanmail.net>
2024-07-06 22:18:41 +0900

    feat: 생성자 추가

--- message-queue/src/main/java/com/example/messagequeue/queue/Message.java
+++ message-queue/src/main/java/com/example/messagequeue/queue/Message.java
@@ -1,8 +1,10 @@
 package com.example.messagequeue.queue;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class Message {
 
     private String id;
