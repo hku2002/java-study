f82a4484d59cc8b6c3302b52980a689e2e8ea82d
hku2002 <hku2002@hanmail.net>
2024-09-10 21:32:50 +0900

    feat: aspect 추가

--- practice-aop/src/main/java/com/aop/sample/PracticeAopApplication.java
+++ practice-aop/src/main/java/com/aop/sample/PracticeAopApplication.java
@@ -1,8 +1,11 @@
 package com.aop.sample;
 
+import com.aop.sample.order.aop.AspectV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import(AspectV1.class)
 @SpringBootApplication
 public class PracticeAopApplication {
 
