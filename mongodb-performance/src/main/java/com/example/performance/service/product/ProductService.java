98f3b69b72b7d54336606d53a531a8857dab18fd
hku2002 <hku2002@hanmail.net>
2024-12-19 22:15:30 +0900

    feat: 상품 insertMany 추가

--- mongodb-performance/src/main/java/com/example/performance/service/product/ProductService.java
+++ mongodb-performance/src/main/java/com/example/performance/service/product/ProductService.java
@@ -69,8 +69,58 @@ public class ProductService {
         log.info("takenTime(ms): {}", takenTime);
     }
 
-    public void insertMany() {
+    public void insertMany(List<Product> products) {
+        MongoDatabase database = mongoClient.getDatabase("performance");
+        MongoCollection<Document> collection = database.getCollection("product");
+        long startTime = System.currentTimeMillis();
 
+        List<Document> productList = new ArrayList<>();
+
+        for (Product product : products) {
+            productList.add(
+                    new Document()
+                            .append("_id", product.getId())
+                            .append("productName", product.getProductName())
+                            .append("price", product.getPrice())
+                            .append("discountPrice", product.getDiscountPrice())
+                            .append("discountType", product.getDiscountType())
+                            .append("category1Id", product.getCategory1Id())
+                            .append("category2Id", product.getCategory2Id())
+                            .append("category3Id", product.getCategory3Id())
+                            .append("category4Id", product.getCategory4Id())
+                            .append("status", product.getStatus())
+                            .append("ThumbnailImageUrl", product.getThumbnailImageUrl())
+                            .append("detailImageUrl", product.getDetailImageUrl())
+                            .append("explainImageUrl", product.getExplainImageUrl())
+                            .append("badgeImageUrl", product.getBadgeImageUrl())
+                            .append("videoUrl", product.getVideoUrl())
+                            .append("stockId", product.getStockId())
+                            .append("stockName", product.getStockName())
+                            .append("option1Id", product.getOption1Id())
+                            .append("option1Name", product.getOption1Name())
+                            .append("option2Id", product.getOption2Id())
+                            .append("option2Name", product.getOption2Name())
+                            .append("isPackage", product.isPackage())
+                            .append("isSample", product.isSample())
+                            .append("activated", product.isActivated())
+                            .append("workerId", product.getWorkerId())
+                            .append("workerName", product.getWorkerName())
+                            .append("eventStartedAt", product.getEventStartedAt())
+                            .append("eventEndedAt", product.getEventEndedAt())
+                            .append("createdAt", product.getCreatedAt())
+                            .append("updatedAt", product.getUpdatedAt())
+            );
+
+        }
+
+        collection.insertMany(productList);
+
+        long endTime = System.currentTimeMillis();
+        long takenTime = (endTime - startTime);
+
+        log.info("startTime    : {}", startTime);
+        log.info("endTime      : {}", endTime);
+        log.info("takenTime(ms): {}", takenTime);
     }
 
     public void bulkWrite() {
--- mongodb-performance/src/test/java/com/example/performance/service/product/ProductServiceTest.java
+++ mongodb-performance/src/test/java/com/example/performance/service/product/ProductServiceTest.java
@@ -56,4 +56,46 @@ class ProductServiceTest {
         productService.insertOne(products);
 
     }
+
+    @Test
+    void insertManyTest() {
+        List<Product> products = new ArrayList<>();
+        for (int i=0; i<100000; i++) {
+            products.add(
+                    Product.builder()
+                            .id((long) i)
+                            .productName("상품명" + i)
+                            .price(1000 + i)
+                            .discountPrice(800 + i)
+                            .category1Id(1L)
+                            .category2Id(2L)
+                            .category3Id(3L)
+                            .category4Id(4L)
+                            .status("SELL")
+                            .thumbnailImageUrl("https://images.sample.com/thumbnail" + i)
+                            .detailImageUrl("https://images.sample.com/detail" + i)
+                            .explainImageUrl("https://images.sample.com/explain" + i)
+                            .badgeImageUrl("https://images.sample.com/badge" + i)
+                            .videoUrl("https://images.sample.com/video" + i)
+                            .stockId((long) i)
+                            .stockName("재고" + i)
+                            .option1Id(1L)
+                            .option1Name("옵션 첫번째")
+                            .option2Id(2L)
+                            .option2Name("옵션 두번재")
+                            .isPackage(false)
+                            .isPackage(false)
+                            .activated(true)
+                            .workerId(1L)
+                            .workerName("작업자명")
+                            .eventStartedAt(LocalDateTime.now().minusHours(3L))
+                            .eventEndedAt(LocalDateTime.now().plusHours(3L))
+                            .createdAt(LocalDateTime.now())
+                            .build()
+            );
+        }
+
+        productService.insertMany(products);
+
+    }
 }
